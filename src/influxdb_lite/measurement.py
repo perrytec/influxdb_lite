c8f877f7b1432a9779314b20bc672298b58b6c17
rodrigo <rodrigo.troncoso@perry.sg>
2022-04-19 17:47:01 -0400

    Added tags and fields params to Measurement class.

--- src/influxdb_lite/measurement.py
+++ src/influxdb_lite/measurement.py
@@ -4,3 +4,5 @@ class Measurement:
     def __init__(self, **kwargs):
         self.name = kwargs.get('name', 'default')
         self.bucket = kwargs.get('bucket', 'default')
+        self.tags = kwargs.get('tags', [])
+        self.fields = kwargs.get('fields', [])
