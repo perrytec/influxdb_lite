dbb8fa1262dd0ab4931bf1e347c22569c3c6d97f
rodrigo <rodrigo.troncoso@perry.sg>
2022-04-21 13:53:17 -0400

    Updated client.range to include absolute and relative intervals.

--- src/influxdb_lite/client.py
+++ src/influxdb_lite/client.py
@@ -1,5 +1,6 @@
 from influxdb_client import InfluxDBClient
 from influxdb_lite.measurement import Measurement
+import datetime as dt
 
 
 class Client(InfluxDBClient):
@@ -32,13 +33,38 @@ class Client(InfluxDBClient):
             range_idx, f'|> filter(fn: (r) => contains(value: r._field, set:{self._parse_list_into_str(_list)}))')
         return self
 
-    def range(self, interval: int):
-        """ Modifies the base query adding a specified range, in this case, {interval} days before the current time. """
+    def range(self, interval: dict):
+        """ Modifies the base query adding a specified range. The format of the interval dictionary depends on the type
+        of range given, this can be either relative or absolute. If the datatype of interval['start'] is a string, the
+        type will relative and if it is a datetime object, it will be considered absolute. A combination of
+        interval['start'], interval['stop'] with different datatype will fail.
+
+        If the range is relative, interval accepts a dictionary in the format {'start': rr_start, 'stop': rr_stop} where
+        rr_start and rr_stop are relative range strings like -15d. If _type is 'absolute', interval accepts a dictionary
+        in the format {'start': dt_start, 'stop': datetime} where dt_start and dt_stop are datetime.datetime timezone
+        unaware objects. If 'stop' key is not present or its value is None, now() will be considered as default. If
+        'start' key is not present method will raise an error. """
         self._validate_selection(['_time'])
         query_list = self.query_str.split('\n')
-        query_list.insert(1, f'|> range(start: -{interval}d)')
+        valid_interval = self._validate_range(interval)
+        query_list.insert(1, f"|> range(start: {valid_interval['start']}, stop: {valid_interval['stop']})")
         self.query_str = '\n'.join(query_list)
         return self
+
+    def _validate_range(self, interval: dict):
+        if interval.get('start', None) is None:
+            raise ValueError(f"Invalid start value. ")
+        elif isinstance(interval['start'], str):
+            pass
+        elif isinstance(interval['start'], dt.datetime):
+            interval['start'] = self._dt_to_RFC3339(interval['start'])
+            interval['stop'] = self._dt_to_RFC3339(interval.get('stop', None))
+        else:
+            raise ValueError(f"_type {type(interval['start'])} not recognized. ")
+
+        if interval.get('stop', None) is None:
+            interval['stop'] = 'now()'
+        return interval
 
     def filter(self, attr_dict: dict):
         """ Adds filter statement to query. Receives an attribute dictionary in the format:
@@ -105,3 +131,27 @@ class Client(InfluxDBClient):
         for column in _list:
             if column not in self.select_list:
                 raise TypeError(f"Please include {column} in the select list.")
+
+    def _dt_to_RFC3339(self, datetime_obj: dt.datetime = dt.datetime.now(), format: str = 'short'):
+        """Transform datetime object into string RFC3339 format (either in date, short or long format). Ignores
+         timezone aware datetime objects. """
+        if datetime_obj is not None:
+            base = datetime_obj.isoformat()
+            res = self._get_resolution(base)
+            base = base.split('+')[0] if '+' in base else base
+            if format == 'date':
+                return base.split('T')[0]
+            elif format == 'short':
+                return base[:-res-1] + 'Z' if res == 6 else base + 'Z'
+            elif format == 'long':
+                return base[:-res//2] + 'Z' if res == 6 else base + '.000Z'
+            else:
+                raise ValueError("Enter a format from 1 to 3")
+        else:
+            return None
+
+    def _get_resolution(self, isoformat):
+        if len(isoformat.split('.')) == 1:
+            return -1
+        else:
+            return len(isoformat.split('.')[1])
