70b7ff4664cdfd712f6207a05bb78eafec3bcb56
rodrigo <rodrigo.troncoso@perry.sg>
2022-04-25 22:00:24 -0400

    Added columns and dict param to Measurement class. Added get_values method.

--- src/influxdb_lite/measurement.py
+++ src/influxdb_lite/measurement.py
@@ -1,4 +1,4 @@
-from influxdb_lite.attributes import Tag, Field
+from influxdb_lite.attributes import Tag, Field, Timestamp, Base
 
 
 class MetaMeasurement(type):
@@ -6,10 +6,23 @@ class MetaMeasurement(type):
         super(MetaMeasurement, cls).__init__(name)
         cls.tags = [attr_name for attr_name in cls.__dict__ if isinstance(cls.__dict__[attr_name], Tag)]
         cls.fields = [attr_name for attr_name in cls.__dict__ if isinstance(cls.__dict__[attr_name], Field)]
-        [cls.__dict__[elem].set_name(elem) for elem in cls.tags + cls.fields]
+        cls.timestamps = [attr_name for attr_name in cls.__dict__ if isinstance(cls.__dict__[attr_name], Timestamp)]
+        cls.columns = cls.tags + cls.fields + cls.timestamps
+        [cls.__dict__[elem].set_name(elem) for elem in cls.tags + cls.fields + cls.timestamps]
+        cls.dict = {k: v for k, v in cls.__dict__.items() if k in cls.columns}
 
 
 class Measurement(metaclass=MetaMeasurement):
     name = ''
     bucket = ''
-    timestamp_label = ''
+    columns = []
+    dict = {}
+
+    def __init__(self, **kwargs):
+        for attribute in kwargs:
+            setattr(getattr(self, attribute), 'value', kwargs[attribute])
+
+    def get_values(self):
+        """Returns a dictionary in the format {column_1: value_1, column_2, value_2, ...} including all the tags,
+        fields and timestamp columns. """
+        return {k: v.value for k, v in self.dict.items()}
